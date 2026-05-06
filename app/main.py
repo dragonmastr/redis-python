5e5b62294aa309c4ffba5ebed1d32538eac7ce91
Hardik Murdia <hardikmurdia@hmurdia-mbp.local>
2026-05-06 17:21:27 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,7 +6,35 @@ def handle_client(connection):
         data = connection.recv(1024)
         if not data:
             break
-        connection.sendall(b"+PONG\r\n")
+        parsed = parse_resp(data)
+        command = parsed[0].upper()
+        if command == "PING":
+            connection.sendall(b"+PONG\r\n")
+        elif command == "ECHO":
+            message = parsed[1]
+            response = f"${len(message)}\r\n{message}\r\n"
+            connection.sendall(response.encode())
+
+def parse_resp(data):
+    parts = data.decode().split("\r\n")
+    print(parts)
+    if not parts[0].startswith("*"):
+        raise ValueError("Invalid RESP Array")
+    element_count = int(parts[0][1:])
+    result = []
+    index = 1
+    for _ in range(element_count):
+        if not parts[index].startswith("$"):
+            raise ValueError("Expected Bulk String")
+        length = int(parts[index][1:])
+        value = parts[index+1]
+        if len(value) != length:
+            raise ValueError("Invalid Bulk String Length")
+        result.append(value)
+        index += 2
+        
+    return result
+
 
 def main():
     print("Logs from your program will appear here!")
--- app/threadtest.py
+++ app/threadtest.py
@@ -17,18 +17,43 @@ import threading, time
 
 # print("All done")
 
-import threading
+# import threading
 
-counter = 0
+# counter = 0
 
-def increment():
-	global counter
-	for _ in range(100_000):
-		#with lock:
-		counter += 1
+# def increment():
+# 	global counter
+# 	for _ in range(100_000):
+# 		#with lock:
+# 		counter += 1
 
-threads = [threading.Thread(target=increment) for _ in range(5)]
-for t in threads: t.start()
-for t in threads: t.join()
+# threads = [threading.Thread(target=increment) for _ in range(5)]
+# for t in threads: t.start()
+# for t in threads: t.join()
 
-print(counter)  # always 500000
+# print(counter)  # always 500000
+
+def parse_resp(data):
+    parts = data.split("\r\n")
+    print(parts)
+    if not parts[0].startswith("*"):
+        raise ValueError("Invalid RESP Array")
+    element_count = int(parts[0][1:])
+    result = []
+    index = 1
+    for _ in range(element_count):
+        if not parts[index].startswith("$"):
+            raise ValueError("Expected Bulk String")
+        length = int(parts[index][1:])
+        value = parts[index+1]
+        if len(value) != length:
+            raise ValueError("Invalid Bulk String Length")
+        result.append(value)
+        index += 2
+        
+    return result
+        
+        
+
+
+parse_resp("*2\r\n$4\r\nECHO\r\n$3\r\nhey\r\n")
